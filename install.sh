37221ef4cd3c0dd35293a6d591ff180936d04d58
Kalleo Pinheiro <windson.rocha@zup.com.br>
2022-08-16 15:06:11 -0300

    necessary adjustments for a better experience

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 echo '########## <updating and upgrade> ##########'
-sudo apt-get update 
+sudo apt-get update
 sudo dpkg --configure -a
 sudo apt-get upgrade -y
 
 echo '########## <updating snap> ##########'
 sudo snap refresh
 
-sudo bash -c 'cat <<EOF > /usr/local/bin/refresh.sh
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt -y autoremove
-sudo apt-get clean
-EOF'
-sudo chmod +x /usr/local/bin/refresh.sh
-
-mkdir -p ~/projects
+echo '########## <creating a Project folder> ##########'
+mkdir -p ~/Projects
 
 echo '########## <installing curl> ##########'
 sudo apt install curl -y -f
@@ -40,9 +32,6 @@ ssh-keygen -t rsa -b 4096 -C "$git_config_user_email"
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
 
-echo 'enabling workspaces for both screens' 
-gsettings set org.gnome.mutter workspaces-only-on-primary false
-
 echo '########## <installing chrome> ##########'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
@@ -51,7 +40,7 @@ rm -rf google-chrome-stable_current_amd64.deb
 
 echo '########## <installing terminator> ##########'
 sudo apt-get update
-sudo apt-get install terminator -y  -f
+sudo apt-get install terminator -y -f
 
 echo '########## <installing docker> ##########'
 sudo apt-get remove docker docker-engine docker.io -y
@@ -65,9 +54,9 @@ docker --version
 sudo chmod 777 /var/run/docker.sock
 
 echo '########## <installing docker-compose> ##########'
-DOCKER_COMPOSE_LATEST=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" \
-   | grep '"tag_name":' \
-   | sed -E 's/.*"([^"]+)".*/\1/')
+DOCKER_COMPOSE_LATEST=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" |
+  grep '"tag_name":' |
+  sed -E 's/.*"([^"]+)".*/\1/')
 sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_LATEST}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
@@ -105,33 +94,56 @@ echo '########## <installing zsh> ##########'
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-echo '########## <installing autosuggestions> ##########' 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-source ~/.zshrc
-
 echo '########## <installing theme> ##########'
 sudo apt install fonts-firacode -y
-wget -O ~/.oh-my-zsh/themes/node.zsh-theme https://raw.githubusercontent.com/skuridin/oh-my-zsh-node-theme/master/node.zsh-theme 
-sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="node"/g' ~/.zshrc
-
-echo '########## <installing meet franz> ##########'
-wget https://github.com/meetfranz/franz/releases/download/v5.1.0/franz_5.1.0_amd64.deb -O franz.deb
-sudo dpkg -i franz.debchristian-kohler.path-intellisense
-sudo apt-get install -y -f 
 
 echo '########## <installing zsh spaceship> ##########'
 sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/themes/spaceship-prompt"
 sudo ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
-source ~/.bashrc
+sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="spaceship"/g' ~/.zshrc
+source ~/.zshrc
 
-echo '########## <installing zsh zplugin> ##########'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+cat <<EOF >>~/.zshrc
 
-source ~/.bashrc
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  vi_mode       # Vi-mode indicator
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+EOF
+source ~/.zshrc
+
+echo '########## <installing zsh zinit> ##########'
+sh -c "$(curl -fsSL https://git.io/zinit-install)"
+zinit self-update
+
+cat <<EOF >>~/.zshrc
+zinit light zdharma/fast-syntax-highlighting
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+EOF
+
+echo '########## <adding alias to zsh> ##########'
+cat <<EOF >>~/.zshrc
+
+alias refresh="sudo apt update; sudo apt -y upgrade; sudo apt -y dist-upgrade; sudo apt -y autoremove; sudo apt clean
+alias projects="cd ~/Projects"
+EOF
 
 echo '########## <installing code> ##########'
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get install apt-transport-https -y
@@ -170,82 +182,92 @@ echo '########## <installing terminator> ##########'
 sudo apt-get update
 sudo apt-get install terminator -y
 
-echo 'adding dracula theme' 
-cat <<EOF >  ~/.config/terminator/config
+echo '########## <customizing terminator> ##########'
+cat <<EOF >>~/.config/terminator/config
 [global_config]
-  title_transmit_bg_color = "#ad7fa8"
+  geometry_hinting = True
+  always_split_with_profile = True
+  title_transmit_bg_color = "#0071c8"
 [keybindings]
-  close_term = <Primary>w
-  close_window = <Primary>q
   new_tab = <Primary>t
-  new_window = <Primary>i
-  paste = <Primary>v
   split_horiz = <Primary>e
   split_vert = <Primary>d
+  close_term = <Primary>w
+  paste = <Primary>v
+  close_window = <Primary>q
   switch_to_tab_1 = <Primary>1
-  switch_to_tab_10 = <Primary>0
   switch_to_tab_2 = <Primary>2
   switch_to_tab_3 = <Primary>3
   switch_to_tab_4 = <Primary>4
   switch_to_tab_5 = <Primary>5
   switch_to_tab_6 = <Primary>6
+  switch_to_tab_10 = <Primary>0
+  new_window = <Primary>i
+[profiles]
+  [[default]]
+    cursor_color = "#aaaaaa"
+  [[Dracula]]
+    background_color = "#1e1f29"
+    background_darkness = 0.7
+    background_type = transparent
+    cursor_color = "#bbbbbb"
+    font = Fira Code weight=453 10
+    foreground_color = "#f8f8f2"
+    scrollback_infinite = True
+    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
+    use_system_font = False
+    copy_on_selection = True
 [layouts]
   [[default]]
     [[[child1]]]
       parent = window0
       type = Terminal
+      profile = Dracula
     [[[window0]]]
       parent = ""
       type = Window
+      profile = Dracula
+      size = 1280, 720
 [plugins]
-[profiles]
-  [[default]]
-    cursor_color = "#aaaaaa"
 EOF
 
-
-cat <<EOF >>  ~/.config/terminator/config
-[[Dracula]]
-    background_color = "#1e1f29"
-    background_darkness = 0.88
-    background_type = transparent
-    copy_on_selection = True
-    cursor_color = "#bbbbbb"
-    foreground_color = "#f8f8f2"
-    palette = "#000000:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#bbbbbb:#555555:#ff5555:#50fa7b:#f1fa8c:#bd93f9:#ff79c6:#8be9fd:#ffffff"
-    scrollback_infinite = True
-EOF
-
-echo '########## <installing nvm> ##########' 
+echo '########## <installing nvm> ##########'
 sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
 
 export NVM_DIR="$HOME/.nvm" && (
-git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-cd "$NVM_DIR"
-git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
 ) && \. "$NVM_DIR/nvm.sh"
+
+cat <<EOF >>~/.zshrc
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+EOF
 
-source ~/.zshrc
 nvm --version
 nvm install 14
 nvm alias default 14
 node --version
 npm --version
 
-echo '########## <installing awscli> ##########' 
+echo '########## <installing awscli> ##########'
 sudo apt-get install awscli -y
 aws --version
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
 session-manager-plugin --version
 
-echo '########## <installing ulauncher> ##########' 
+echo '########## <installing gnome tweeks> ##########'
+sudo apt install gnome-tweaks
+gsettings set org.gnome.mutter center-new-windows true
+
+echo '########## <installing ulauncher> ##########'
 sudo add-apt-repository ppa:agornostal/ulauncher
 sudo apt update
 sudo apt install ulauncher
+sudo apt install wmctrl
 
 echo '########## <set favortie apps> ##########'
 dconf write /org/gnome/shell/favorite-apps "[ \
@@ -253,24 +275,41 @@ dconf write /org/gnome/shell/favorite-apps "[ \
   'google-chrome.desktop', \
   'insomnia.desktop', \
   'spotify.desktop', \
-  'code.desktop' \
-]" 
+  'code.desktop' \,
+  'terminator.desktop' \,
+  'bitwarden.desktop' \
+]"
+
+echo '########## <enabling workspaces for both screens> ##########'
+sudo apt install dconf-editor
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
+# gsettings reset org.gnome.shell.extensions.dash-to-dock dash-max-icon-size
 
 echo '########## <configure git alias> ##########'
 git config --global alias.l "!git log --pretty=format:'%C(blue)%h%C(red)%d %C(white) %s %C(cyan)[%cn] %C(green)%cr'"
 git config --global alias.c "!git add . && git commit"
 git config --global alias.s "!git status -sb"
 git config --global alias.branches "!git branch -a"
-git config --global alias.amend = !git add . && git commit --amend --no-edit
+git config --global alias.amend "!git add . && git commit --amend --no-verify"
+git config --global alias.fetch "!git fetch --all"
 
 echo '########## <installing npm global modules> ##########'
 npm install --global yarn
+npm install -g npm-check-updates
+npm install -g ntl
+
+source ~/.zshrc
 
 echo "########## --> That’s all folks! <-- ##########"
 echo "Restart computer for you? (y/n)"
 read restart_computer
-if echo "$restart_computer" | grep -iq "^y" ;then
-	sudo shutdown -r 0
+if echo "$restart_computer" | grep -iq "^y"; then
+  sudo shutdown -r 0
 else
-	echo "Okay. Good work! :)"
+  echo "Okay. Good work! :)"
 fi
